2a536d71e2506cb798e079164be46ad1d3644883
Chris Salinas Jr <chrissalinasjr@gmail.com>
2023-12-17 00:48:48 -0700

    chore: remove commented themes

--- src/components/GitHub.jsx
+++ src/components/GitHub.jsx
@@ -5,8 +5,6 @@ import { Comment, Function, Section } from "./";
 
 const explicitTheme = {
   dark: ["#383838", "#C586C0", "#4EC9B0", "#DCDCAA", "#4FC1FF"],
-  // dark: ['#383838', '#4e5e6a', '#486982', '#3575a5', '#0078d4'],
-  // dark: ['#383838', '#525c4e', '#617657', '#648555', '#6a9955'],
 };
 
 export const GitHub = () => (
