348cd17a7baf9bbfd2eae3b5b02163555bcd2f76
Chris Salinas Jr <chrissalinasjr@gmail.com>
2024-01-21 01:18:17 -0700

    chore: remove unused code

--- src/pages/wordle/context/reducer/addLetter.js
+++ src/pages/wordle/context/reducer/addLetter.js
@@ -1,21 +1,12 @@
-// import { findIndex } from "lodash";
-// import Status from "../../Status";
 import { updateBoard } from "./helpers";
 
 export const addLetter = (state, letter) => {
-  const { board, keyboard, row, guess } = state;
+  const { board, row, guess } = state;
 
   // Do not add more than 5 letters
   if (guess.length >= 5) {
     return state;
   }
-
-  // Check if letter is valid
-  // const keyIdx = findIndex(keyboard, (k) => k.label === letter);
-  // const key = keyboard[keyIdx];
-  // if (key.status === Status.Absent) {
-  //   return state;
-  // }
 
   // Update Guess
   const newGuess = guess + letter;
