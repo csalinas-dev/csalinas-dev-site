336b1fcd11d04fbc53a9f27718813e59fc364d4f
Chris Salinas Jr <chrissalinasjr@gmail.com>
2024-02-25 23:43:41 -0700

    feat(hashtag): win detection
    Closes #46

--- src/pages/hashtag/Tile.jsx
+++ src/pages/hashtag/Tile.jsx
@@ -9,12 +9,15 @@ const Container = styled.div`
   background-color: var(--selectionBackground);
   border-radius: 10%;
   box-shadow: 0.025rem 0.05rem 0.2rem rgba(0, 0, 0, 0.5);
-  cursor: move;
   display: flex;
   justify-content: center;
   overflow: hidden;
   text-shadow: 1px 1px var(--background);
   user-select: none;
+
+  &.draggable {
+    cursor: grab;
+  }
 
   &.absent {
     background-color: var(--absentBackground);
@@ -33,7 +36,12 @@ const Container = styled.div`
 
 export const Tile = ({ tile, index }) => {
   const { letter, status: className } = tile;
-  const { dispatch } = useContext(Context);
+  const {
+    state: { win },
+    dispatch,
+  } = useContext(Context);
+
+  const props = { className };
 
   const onDragStart = useCallback(
     () => dispatch(dragging(index)),
@@ -45,17 +53,13 @@ export const Tile = ({ tile, index }) => {
     [dispatch, index]
   );
 
-  return (
-    <Container
-      {...{
-        className,
-        draggable: true,
-        onDragStart,
-        onDragOver,
-        onDrop,
-      }}
-    >
-      {letter}
-    </Container>
-  );
+  if (win === null) {
+    props.className += " draggable";
+    props.draggable = true;
+    props.onDragStart = onDragStart;
+    props.onDragOver = onDragOver;
+    props.onDrop = onDrop;
+  }
+
+  return <Container {...props}>{letter}</Container>;
 };
--- src/pages/hashtag/context/reducer/swapTiles.js
+++ src/pages/hashtag/context/reducer/swapTiles.js
@@ -1,17 +1,59 @@
 import { cloneDeep } from "lodash";
 import { updateBoardStatuses } from "./helpers";
+import Status from "pages/wordle/Status";
 
-export const swapTiles = ({ tileInHand, board, ...state }, tileDroppedOn) => {
+export const swapTiles = (state, tileDroppedOn) => {
+  const { tileInHand, board } = state;
+  if (tileInHand === tileDroppedOn) {
+    // TODO: Error "You can't swap a tile with itself."
+    //   Perhaps this error shakes the tile in hand.
+    return state;
+  }
+
+  if (
+    board[tileInHand].status === Status.Correct ||
+    board[tileDroppedOn].status === Status.Correct
+  ) {
+    // TODO: Error "You can't swap a correct tile."
+    //   Perhaps this error shakes the tile in hand.
+    return state;
+  }
+
+  const moves = state.moves + 1;
+  if (moves >= 12) {
+    return {
+      ...state,
+      moves,
+      tileInHand: null,
+      win: false,
+      board: state.target,
+    };
+  }
+
   const newBoard = cloneDeep(board);
   newBoard[tileInHand].letter = board[tileDroppedOn].letter;
   newBoard[tileDroppedOn].letter = board[tileInHand].letter;
 
   const updatedBoard = updateBoardStatuses(state, newBoard);
 
+  // Check if the board is complete
+  const isComplete = updatedBoard.every(
+    (tile) => !tile || tile.status === Status.Correct
+  );
+  if (isComplete) {
+    return {
+      ...state,
+      moves,
+      tileInHand: null,
+      win: true,
+      board: updatedBoard,
+    };
+  }
+
   return {
     ...state,
-    board: updatedBoard,
+    moves,
     tileInHand: null,
-    moves: state.moves + 1,
+    board: updatedBoard,
   };
 };
