9f35a8f3c9968306ad0b1528d76cd23deb473880
Chris Salinas Jr <chrissalinasjr@gmail.com>
2024-07-23 00:44:14 -0600

    feat(projects): convert implementation

---
+++ src/lib/hygraph.js
@@ -0,0 +1,8 @@
+import { GraphQLClient } from "graphql-request";
+const endpoint = process.env.HYGRAPH_ENDPOINT;
+const client = new GraphQLClient(endpoint, {
+  headers: {
+    authorization: `Bearer ${process.env.HYGRAPH_TOKEN}`,
+  },
+});
+export default client;
