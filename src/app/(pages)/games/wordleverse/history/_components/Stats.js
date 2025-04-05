222bab2508703ccce6289634f42b7f35204bd35f
Chris Salinas Jr <chrissalinasjr@gmail.com>
2025-04-05 03:01:12 -0600

    fix(stats): wrapping

--- src/app/(pages)/games/wordleverse/history/_components/Stats.js
+++ src/app/(pages)/games/wordleverse/history/_components/Stats.js
@@ -5,6 +5,7 @@ import styled from "@emotion/styled";
 // Styled components
 const StatsContainer = styled.div`
   display: flex;
+  flex-flow: row wrap;
   justify-content: center;
   gap: 2rem;
   margin-bottom: 2rem;
